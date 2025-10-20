3dbfe669225845c0e50ef556236aa0048b2d7927
samuel-acevedo26 <samuelacebedo26@gmail.com>
2025-10-20 15:29:40 -0500

    Add files via upload

---
+++ 1-escenario-interactivo/js/js.js
@@ -0,0 +1,5 @@
+
+const monedas = document.querySelectorAll(".mon")
+const ardillas = document.querySelectorAll(".ard")
+const contador = document.querySelector(".score")
+const contar = document.querySelector(".score2")

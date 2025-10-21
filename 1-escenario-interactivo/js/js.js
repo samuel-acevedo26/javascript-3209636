440f5ec13d8bc2023eb949b62af8ef12f41f5c08
samuel-acevedo26 <samuelacebedo26@gmail.com>
2025-10-21 15:28:20 -0500

    Add files via upload

--- 1-escenario-interactivo/js/js.js
+++ 1-escenario-interactivo/js/js.js
@@ -3,3 +3,57 @@ const monedas = document.querySelectorAll(".mon")
 const ardillas = document.querySelectorAll(".ard")
 const contador = document.querySelector(".score")
 const contar = document.querySelector(".score2")
+let i = 0
+let j = 0
+
+console.log(monedas)
+console.log(ardillas)
+console.log(contador)
+console.log(contar)
+
+
+ardillas.forEach( item => {
+    /* console.log("Elemento: ", item)
+     */
+
+    item.addEventListener("click", () => {
+item.classList.add("saltar")
+
+j++
+contar.textContent = `SCORE = ${j}`
+
+    })
+
+
+
+item.addEventListener("animationend", () => {
+    item.style.display = "none"
+}, {once: true})
+
+})
+
+
+monedas.forEach( item => {
+    /* console.log("Elemento: ", item)
+     */
+
+    item.addEventListener("click", () => {
+item.classList.add("saltar")
+
+i++
+contador.textContent = `SCORE = ${i}`
+
+
+
+    })
+
+item.addEventListener("animationend", () => {
+    item.style.display = "none"
+}, {once: true})
+
+
+})
+
+
+
+
